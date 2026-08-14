681b44f64d4f6715748b20ce04d8ada3d90eac39
Genesis Developer <maelguitar2026@gmail.com>
2026-08-14 14:55:50 -0300

    Criação do AmpKnob.h e AmpKnob.cpp

---
+++ src/ui/AmpKnob.h
@@ -0,0 +1,31 @@
+#ifndef AMPKNOB_H
+#define AMPKNOB_H
+
+#include <QWidget>
+#include <QMouseEvent>
+#include <QPainter>
+
+class AmpKnob : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit AmpKnob(QWidget *parent = nullptr);
+
+    int value() const;
+    void setValue(int value);
+
+signals:
+    void valueChanged(int value);
+
+protected:
+    void painEvent(QPainEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMoveEvent *event) override;
+
+private:
+   int m_value = 50;
+   int m_lastMouseY = 0;
+};
+
+#endif // AMPKNOB_H
